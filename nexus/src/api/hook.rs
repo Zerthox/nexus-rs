21bee27ec331b8694e077866f89b18410d0bd70c
Zerthox <zerthox@gmail.com>
2024-03-16 15:56:00 +0100

    Start implementing hook api

--- nexus/src/api/hook.rs
+++ nexus/src/api/hook.rs
@@ -1,21 +1,50 @@
 use std::ffi::c_void;
 
+/// MinHook error codes.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(C)]
 pub enum HookStatus {
+    /// Unknown error. Should not be returned.
     Unknown = -1,
+
+    /// Successful.
     Ok = 0,
+
+    /// MinHook is already initialized.
     ErrorAlreadyInitialized,
+
+    /// MinHook is not initialized yet, or already uninitialized.
     ErrorNotInitialized,
+
+    /// The hook for the specified target function is already created.
     ErrorAlreadyCreated,
+
+    /// The hook for the specified target function is not created yet.
     ErrorNotCreated,
+
+    /// The hook for the specified target function is already enabled.
     ErrorEnabled,
+
+    /// The hook for the specified target function is not enabled yet, or already disabled.
     ErrorDisabled,
+
+    /// The specified pointer is invalid.
+    /// It points the address of non-allocated and/or non-executable region.
     ErrorNotExecutable,
+
+    /// The specified target function cannot be hooked.
     ErrorUnsupportedFunction,
+
+    /// Failed to allocate memory.
     ErrorMemoryAlloc,
+
+    /// Failed to change the memory protection.
     ErrorMemoryProtect,
+
+    /// The specified module is not loaded.
     ErrorModuleNotFound,
+
+    /// The specified function is not found.
     ErrorFunctionNotFound,
 }
 
@@ -30,3 +59,70 @@ pub type RawHookRemove = unsafe extern "stdcall-unwind" fn(target: *const c_void
 pub type RawHookEnable = unsafe extern "stdcall-unwind" fn(target: *const c_void) -> HookStatus;
 
 pub type RawHookDisable = unsafe extern "stdcall-unwind" fn(target: *const c_void) -> HookStatus;
+
+/// MinHook bindings using trait interfaces from the [detour](https://github.com/darfink/detour-rs) crate.
+#[cfg(feature = "hook")]
+mod bindings {
+    use super::*;
+    use crate::addon_api;
+    use detour::{Function, HookableWith};
+    use std::ptr;
+
+    /// Creates a hook for the specified target function, in **disabled** state.
+    ///
+    /// Returns a pointer to the trampoline function, which will be used to call the original target function.
+    #[inline]
+    pub fn create_hook<F, D>(target: F, detour: D) -> Result<*const (), HookStatus>
+    where
+        F: Function + HookableWith<D>,
+        D: Function,
+    {
+        let mut original = ptr::null();
+        let create = addon_api().hook_create;
+        let err = unsafe { create(target.to_ptr() as _, detour.to_ptr() as _, &mut original) };
+        match err {
+            HookStatus::Ok => Ok(original as _),
+            _ => Err(err),
+        }
+    }
+
+    /// Creates a hook for the specified target function, and enables it if successful.
+    ///
+    /// Returns a pointer to the trampoline function, which will be used to call the original target function.
+    #[inline]
+    pub fn create_hook_enabled<F, D>(target: F, detour: D) -> Result<*const (), HookStatus>
+    where
+        F: Function + HookableWith<D>,
+        D: Function,
+    {
+        let result = create_hook(target, detour);
+        if result.is_ok() {
+            enable_hook(target);
+        }
+        result
+    }
+
+    /// Removes an already created hook.
+    #[inline]
+    pub fn remove_hook(target: impl Function) -> HookStatus {
+        let remove = addon_api().hook_remove;
+        unsafe { remove(target.to_ptr() as _) }
+    }
+
+    /// Enables an already created hook.
+    #[inline]
+    pub fn enable_hook(target: impl Function) -> HookStatus {
+        let enable = addon_api().hook_enable;
+        unsafe { enable(target.to_ptr() as _) }
+    }
+
+    /// Disables an already created hook.
+    #[inline]
+    pub fn disable_hook(target: impl Function) -> HookStatus {
+        let disable = addon_api().hook_disable;
+        unsafe { disable(target.to_ptr() as _) }
+    }
+}
+
+#[cfg(feature = "hook")]
+pub use bindings::*;
