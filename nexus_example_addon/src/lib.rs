a4998c0fb9b1f27351abead8b9a866d4ab5ce8d0
Zerthox <zerthox@gmail.com>
2024-03-16 15:57:00 +0100

    Update example

--- nexus_example_addon/src/lib.rs
+++ nexus_example_addon/src/lib.rs
@@ -1,10 +1,28 @@
-use nexus::{AddonFlags, UpdateProvider};
+use nexus::{
+    gui::{register_render, RenderType},
+    imgui::Window,
+    AddonFlags, UpdateProvider,
+};
 
 nexus::export! {
     signature: -0x12345678, // raidcore addon id or NEGATIVE random unique signature
-    load: || {},
+    load,
     unload: || {},
     flags: AddonFlags::None,
     provider: UpdateProvider::GitHub,
     update_link: "https://github.com/zerthox/nexus-rs",
+}
+
+fn load() {
+    let mut show = false;
+    register_render(RenderType::Render, move |ui| {
+        Window::new("Test window").build(ui, || {
+            if show {
+                show = !ui.button("hide");
+                ui.text("Hello world");
+            } else {
+                show = ui.button("show");
+            }
+        });
+    });
 }
