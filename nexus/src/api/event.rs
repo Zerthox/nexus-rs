4606481b64bba1efe4a70aea7f37639701c623b6
Zerthox <zerthox@gmail.com>
2024-03-26 18:09:34 +0100

    Adjust event api

--- nexus/src/api/event.rs
+++ nexus/src/api/event.rs
@@ -1,15 +1,22 @@
 use crate::{addon_api, revertible::Revertible, util::str_to_c, AddonApi};
-use std::ffi::{c_char, c_void};
+use std::{
+    ffi::{c_char, c_void},
+    mem,
+};
 
-pub type RawEventConsume = extern "C-unwind" fn(event_args: *const c_void);
+pub type RawEventConsume<T> = extern "C-unwind" fn(event_args: *const T);
+
+pub type RawEventConsumeUnknown = RawEventConsume<c_void>;
 
 pub type RawEventRaise =
     unsafe extern "C-unwind" fn(identifier: *const c_char, event_data: *const c_void);
 
 pub type RawEventRaiseNotification = unsafe extern "C-unwind" fn(identifier: *const c_char);
 
-pub type RawEventSubscribe =
-    unsafe extern "C-unwind" fn(identifier: *const c_char, consume_callback: RawEventConsume);
+pub type RawEventSubscribe = unsafe extern "C-unwind" fn(
+    identifier: *const c_char,
+    consume_callback: RawEventConsumeUnknown,
+);
 
 /// Mumble identity update event identifier.
 pub const MUMBLE_IDENTITY_UPDATED: &str = "EV_MUMBLE_IDENTITY_UPDATED";
@@ -17,12 +24,12 @@ pub const MUMBLE_IDENTITY_UPDATED: &str = "EV_MUMBLE_IDENTITY_UPDATED";
 /// Window resized event identifier.
 pub const WINDOW_RESIZED: &str = "EV_WINDOW_RESIZED";
 
-/// Subscribes to an event with a raw callback.
+/// Subscribes to an event with a raw callback using an unknown payload.
 ///
 /// Returns a [`Revertible`] to revert the subscribe.
-pub fn event_subscribe_raw(
+pub fn event_subscribe_unknown(
     identifier: impl AsRef<str>,
-    callback: RawEventConsume,
+    callback: RawEventConsumeUnknown,
 ) -> Revertible<impl Fn() + Send + Sync + Clone + 'static> {
     let identifier = str_to_c(identifier, "failed to convert event identifier");
     let AddonApi {
@@ -35,8 +42,30 @@ pub fn event_subscribe_raw(
     revert.into()
 }
 
+/// Subscribes to an event with a raw callback using a typed payload.
+///
+/// Returns a [`Revertible`] to revert the subscribe.
+///
+/// # Safety
+/// The caller must guarantee that the passed event identifier comes with valid data of the given type.
+pub unsafe fn event_subscribe_typed<T>(
+    identifier: impl AsRef<str>,
+    callback: RawEventConsume<T>,
+) -> Revertible<impl Fn() + Send + Sync + Clone + 'static> {
+    let identifier = str_to_c(identifier, "failed to convert event identifier");
+    let AddonApi {
+        event_subscribe,
+        event_unsubscribe,
+        ..
+    } = addon_api();
+    let callback = unsafe { mem::transmute(callback) };
+    unsafe { event_subscribe(identifier.as_ptr(), callback) };
+    let revert = move || unsafe { event_unsubscribe(identifier.as_ptr(), callback) };
+    revert.into()
+}
+
 /// Unsubscribes a previously registered raw event callback.
-pub fn event_unsubscribe_raw(identifier: impl AsRef<str>, callback: RawEventConsume) {
+pub fn event_unsubscribe(identifier: impl AsRef<str>, callback: RawEventConsumeUnknown) {
     let identifier = str_to_c(identifier, "failed to convert event identifier");
     let AddonApi {
         event_unsubscribe, ..
@@ -45,49 +74,60 @@ pub fn event_unsubscribe_raw(identifier: impl AsRef<str>, callback: RawEventCons
 }
 
 /// Macro to subscribe to an event with a wrapped callback.
+/// This macro is [unsafe](https://doc.rust-lang.org/std/keyword.unsafe.html).
 ///
 /// Returns a callable that reverts the subscribe.
 ///
 /// # Usage
 /// ```no_run
 /// # use nexus::event::*;
-/// event_subscribe!(
-///     "MY_EVENT" => i32,
-///     |args| println!("Received {args:?}"),
-/// );
+/// unsafe {
+///     event_subscribe!("MY_EVENT" => i32, |args| {
+///         println!("Received {args:?}");
+///     })
+/// }.revert_on_unload();
 /// ```
 ///
-/// The event name and callback can also be dynamic.
+/// The event identifier may be dynamic and the callback may be a function name.
 /// ```no_run
 /// # use nexus::event::*;
 /// let event: &str = "MY_EVENT";
 /// fn event_callback(event_args: Option<&i32>) {
 ///     println!("Received {event_args:?}");
 /// }
-/// let revertible = event_subscribe!(event => i32, event_callback);
+/// let revertible = unsafe { event_subscribe!(event => i32, event_callback) };
 /// revertible.revert();
 /// ```
-// TODO: optionally allow captures by storing a dyn FnMut
+///
+/// The `unsafe` keyword may be moved into the macro call:
+/// ```no_run
+/// # use nexus::event::*;
+/// # fn event_callback(_: Option<&()>) {}
+/// event_subscribe!(unsafe "MY_EVENT" => (), event_callback);
+/// ```
+///
+/// # Safety
+/// See [`event_subscribe_typed`].
 #[macro_export]
 macro_rules! event_subscribe {
-    ( $event:expr , $ty:ty , dyn $callback:expr $(,)? ) => {
-        $crate::event::subscribe!($event => $ty, dyn $callback)
+    ( unsafe $event:expr , $ty:ty , $callback:expr $(,)? ) => {
+        unsafe { $crate::event::event_subscribe!($event => $ty, $callback) }
+    };
+    ( unsafe $event:expr => $ty:ty , $callback:expr $(,)? ) => {
+        unsafe { $crate::event::event_subscribe!($event => $ty, $callback) }
     };
     ( $event:expr , $ty:ty , $callback:expr $(,)? ) => {
-        $crate::event::subscribe!($event => $ty, $callback)
+        $crate::event::event_subscribe!($event => $ty, $callback)
     };
-    ( $event:expr => $ty:ty , dyn $callback:expr $(,)? ) => {{
-        todo!("dynamic event subscribe callback")
-    }};
     ( $event:expr => $ty:ty , $callback:expr $(,)? ) => {{
-        const CALLBACK: fn(::std::option::Option<&$ty>) = $callback;
+        const __CALLBACK: fn(::std::option::Option<&$ty>) = $callback;
 
-        extern "C-unwind" fn event_callback_wrapper(data: *const ::std::ffi::c_void) {
-            let data = data as *const $ty;
-            CALLBACK(unsafe { data.as_ref() })
+        extern "C-unwind" fn __event_callback_wrapper(data: *const $ty) {
+            let data = unsafe { data.as_ref() };
+            __CALLBACK(data)
         }
 
-        $crate::event::event_subscribe_raw($event, event_callback_wrapper)
+        $crate::event::event_subscribe_typed($event, __event_callback_wrapper)
     }};
 }
 
